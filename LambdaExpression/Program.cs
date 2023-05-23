4267ddce59ce01788f06d581603ccd1333d684e6
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-05-23 03:11:17 +0200

    Lambda Expression with Predicate code examples

---
+++ LambdaExpression/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LambdaExpression
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello Lambda");
+
+            var strings = new List<string> { "omar", "ali", "ziad", "ahmed" };
+            var filters = strings.FindAll(s => s.Length == 4);
+            foreach ( var filter in filters )
+            {
+                Console.WriteLine(filter);
+            }
+        }
+    }
+}
